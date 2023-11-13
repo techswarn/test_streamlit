3019955c75e56a5b0679b0c0d11bae8d9085de97
legaltextai <144342123+legaltextai@users.noreply.github.com>
2023-11-13 10:01:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+
+st.write("This is a very simple streamlit app that does nothing") 
+
+st.text_input("enter your input here, expect nothing)
